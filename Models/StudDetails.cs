8f0f8130ff6e3d8022653bc0f0299eadcaf18994
harsitagunasekaran.07@gmail.com <harsitagunasekaran.07@gmail.com>
2021-06-03 16:52:00 +0530

    Add project files.

---
+++ Models/StudDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentDetails.Models
+{
+    public class StudDetails
+    {
+        public int Roll_Number { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+    }
+}
